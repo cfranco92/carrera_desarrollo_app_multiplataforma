84636d61125babe40ec15baaa81723fac1343975
cfrancobedoya <cfrancobedoya@gmail.com>
2017-06-30 21:20:57 -0500

    ESTRUCTURAS DE DATOS INMUTABLES
    La inmutabilidad de los objetos es algo muy importante para la programación funcional.
    El operador === nos ayuda a comparar objetos, ejecutando la comparación no directamente a los datos del objeto sino, a la referencia del objeto.
    Cuando asignamos un objeto a otro estamos haciendo que ambos apunten a la misma referencia, por eso al modificar un objeto el otro también se verá afectado, porque ambos tienen la misma referencia de memoria.
    La inmutabilidad tiene por objetivo hacer que los parámetros de un objeto sean no modificables o inmutables
    Si quieres aprender más sobre inmutabilidad de objetos en JavaScript en la sección de Enlaces dejamos el link de una librería diseñada por Facebook que nos permite crear distintos tipos de objetos inmutables
    
    Link: --> https://facebook.github.io/immutable-js/

---
+++ 1 fundamentos_Javascript/7 Estructuras de datos y funciones avanzadas/inmutable.js
@@ -0,0 +1,21 @@
+let sacha = {nombre: 'Sacha', apellido: 'Franco', edad: 26 }
+
+let otroSacha = sacha
+otroSacha === sacha
+
+function cumpleano(sacha){
+  persona.edad++
+}
+sacha
+
+cumpleanos = function (persona){
+  contador++
+  persona.edad++
+}
+contador
+
+cumpleanos = function (persona){
+  const clone = Object().assign({}, persona)
+  clone .edad++
+  return clone
+}
