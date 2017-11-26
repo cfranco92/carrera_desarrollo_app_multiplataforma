be1152031fb369f6769e4edea5dbe24f9617b5e8
Cristian Franco <cfrancobedoya@gmail.com>
2017-11-25 19:09:42 -0500

    CAMBIOS EN EL CURSO
    Se hace un seguimiento continuo a los videos.
    Se van agregando comentarios, al seguimiento de cada parte del curso.
    
    Lo principal de este curso, es ver algunos conceptos de ingeniería de software,
    y prepararnos para el desarrollo en Xamarin.

---
+++ 2 c#/BackEnd/BackEnd/Controllers/BackEndContext.asmx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+
+namespace BackEnd.Controllers
+{
+    public class BackEndContext : System.Web.Services.WebService
+    {
+
+    }
+}
---
+++ 2 c#/BackEnd/BackEnd/Models/Apprentice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BackEnd.Models
+{
+    public class Apprentice
+    {
+        int id;
+
+        public int Id{
+            set; get;
+        }
+
+
+
+        //public void SetId(int i){
+        //    id = 1;
+        //}
+
+        //public int GetId(){
+        //    return id;
+        //}
+    }
+}
